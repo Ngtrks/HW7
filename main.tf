ebb1d23228d00ef307932923429a73d7387df2e8
Ngtrks <ngtrks@mail.ru>
2022-01-19 22:58:53 +0300

    Added Terraform and Playbooks

---
+++ main.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.68.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "AQAAAAA1di2XAATuwf68SVRTnk4jnNXFDfKWb8g"
+  cloud_id  = "b1gh5f03ge98a9hgsj22"
+  folder_id = "b1gpmpceae0ms84kvank"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80viupr3qjr5g6g9du"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("C:/users/anton/yandex-cloud-terraform/meta.txt")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "terraform2"
+
+  resources {
+    cores  = 4
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80viupr3qjr5g6g9du"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("C:/users/anton/yandex-cloud-terraform/meta.txt")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
+
